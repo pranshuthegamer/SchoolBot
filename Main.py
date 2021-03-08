e1d8fca39a9abd9bf05ada61a3c224502dd4887f
PranshuTG <12345qwertyman12345@gmail.com>
2021-03-08 14:03:47 +0530

    init

---
+++ Main.py
@@ -0,0 +1,40 @@
+import discord
+from discord.ext import commands
+prefix = "-"
+
+ 
+client = discord.Client()
+bot = commands.Bot(command_prefix=prefix)
+
+
+@bot.event
+async def on_message(context):
+    if context.content.startswith('ping'):
+        await context.channel.send('pong')
+    print(context.author," : ",context.content)
+    await bot.process_commands(context)
+
+
+
+@bot.command(name='mute')
+async def mute(context, member : discord.Member):
+    sender:discord.Member = context.author
+    checkrole1 = discord.utils.get(context.guild.roles, name = "Mod")
+    if checkrole1 in sender.roles:
+        role = discord.utils.get(context.guild.roles, name="Muted")
+        await context.channel.send(context.author.name + " Muted " + str(member.name))
+        await member.add_roles(role)
+
+@bot.command(name='unmute')
+async def unmute(context, member : discord.Member):
+    sender:discord.Member = context.author
+    checkrole1 = discord.utils.get(context.guild.roles, name = "Mod")
+    if checkrole1 in sender.roles:
+        role = discord.utils.get(context.guild.roles, name="Muted")
+        await context.channel.send(context.author.name + " unmuted " + str(member.name))
+        await member.remove_roles(role)
+
+
+
+
+bot.run('ODE3Njg1NjkxMDgwNzY5NTg3.YENHJA.T3oLFGv6KXgkwiA5BTWgHuXf8Go')
