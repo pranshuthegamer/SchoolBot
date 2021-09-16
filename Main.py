c96ab1579b97e9adf5c2a28b5740c90fcbac5713
pranshuthegamer <61378895+pranshuthegamer@users.noreply.github.com>
2021-09-16 05:34:59 +0000

    error error

--- Main.py
+++ Main.py
@@ -226,7 +226,7 @@ async def vcmuteall(ctx):
     elif checkrole1 == False:
           await ctx.channel.send("hey you dont have a mod role!")
 
-@vcmute.error
+@vcmuteall.error
 async def error(ctx,error):
   await error_handler(ctx,error)
 
@@ -244,7 +244,7 @@ async def vcunmuteall(ctx):
   elif checkrole1 == False:
     await ctx.channel.send("hey you dont have a mod role!")
 
-@vcunmute.error
+@vcunmuteall.error
 async def error(ctx,error):
   await error_handler(ctx,error)
 
